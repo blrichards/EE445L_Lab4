b9105ce419b0d0b2fbbffd8429e5d668e3418f39
Ryan Mitchell <rmitchell97@utexas.edu>
2018-09-24 16:15:36 -0500

    Updated Init for Buttons

--- Source/Buttons.c
+++ Source/Buttons.c
@@ -20,24 +20,27 @@
 #define PF2 (*((volatile uint32_t*)0x40025010))
 
 void Buttons_Init(void)
-{                      
-  GPIO_PORTE_DIR_R &= ~0x0F;    // Set dir to input 0-3
-  GPIO_PORTE_AFSEL_R &= ~0x0F;  // disable alt funct
-  GPIO_PORTE_DEN_R |= 0x0F;     //     enable digital I/O
-  GPIO_PORTE_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
-  GPIO_PORTE_AMSEL_R = 0;       //     disable analog functionality on PF
-  GPIO_PORTE_IS_R &= ~0x0F;     // is edge-sensitive
-  GPIO_PORTE_IBE_R &= ~0x0F;    // PF4 is not both edges
-  GPIO_PORTE_IEV_R |= ~0x0F;    // PF4 rising edge event
-  GPIO_PORTE_ICR_R = 0x0F;      // clear flag
-  GPIO_PORTE_IM_R |= 0x0F;      // arm interrupt on PF4 *** No IME bit as mentioned in Book ***
+{
+  volatile uint32_t delay;
+  SYSCTL_RCGCGPIO_R |= 0x00000002;  // 1) activate clock for Port B
+  delay = SYSCTL_RCGCGPIO_R;        // allow time for clock to start
+  GPIO_PORTB_DIR_R &= ~0x0F;    // Set dir to input 0-3
+  GPIO_PORTB_AFSEL_R &= ~0x0F;  // disable alt funct
+  GPIO_PORTB_DEN_R |= 0x0F;     //     enable digital I/O
+  GPIO_PORTB_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
+  GPIO_PORTB_AMSEL_R = 0;       //     disable analog functionality on PF
+  GPIO_PORTB_IS_R &= ~0x0F;     // is edge-sensitive
+  GPIO_PORTB_IBE_R &= ~0x0F;    // PF4 is not both edges
+  GPIO_PORTB_IEV_R |= ~0x0F;    // PF4 rising edge event
+  GPIO_PORTB_ICR_R = 0x0F;      // clear flag
+  GPIO_PORTB_IM_R |= 0x0F;      // arm interrupt on PF4 *** No IME bit as mentioned in Book ***
   NVIC_PRI1_R = (NVIC_PRI1_R&0xFFFFFF00)|0x000000E0; // (g) priority 5
-  NVIC_EN0_R = 0x00000010;      // (h) enable interrupt 30 in NVIC
+  NVIC_EN0_R = 0x00000002;      // (h) enable interrupt 30 in NVIC
 }
 
-void GPIOPortE_Handler(void){
-	uint8_t flags = (GPIO_PORTE_MIS_R & 0x0F);
-	GPIO_PORTE_ICR_R = 0x0F;      // acknowledge flag
+void GPIOPortB_Handler(void){
+	uint8_t flags = (GPIO_PORTB_MIS_R & 0x0F);
+	GPIO_PORTB_ICR_R = 0x0F;      // acknowledge flag
 
 	switch(flags){
 		case FAR_LEFT_SWITCH:
